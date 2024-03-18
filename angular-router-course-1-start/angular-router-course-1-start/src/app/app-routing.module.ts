ed91dc73f5a15c06f44c15bcb93fcb7982cc33a1
feliperod0519@hotmail.com <juan_andres_rodriguez_0519@hotmail.com>
2024-03-17 22:45:05 -0400

    lazy routing

--- angular-router-course-1-start/angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-router-course-1-start/angular-router-course-1-start/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-
+  { path: 'courses', loadChildren: ()=> { return import('./courses/courses.module').then(module=>module.CoursesModule) } },
+  { path: "login", component: LoginComponent},
+  { path: "about", component: AboutComponent},
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
